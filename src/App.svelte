d3a97a8222b97a7b298d05c04157a68111935e75
robert-virtual <robertocastill63@gmail.com>
2022-10-15 10:29:39 -0600

    init

---
+++ src/App.svelte
@@ -0,0 +1,35 @@
+<script lang="ts">
+</script>
+
+<main>
+  <h1>Robert Castillo</h1>
+
+  <div class="card">
+    <h1>Hola mundo</h1>
+  </div>
+
+  <p>
+    pagina web de roberto Castilllo
+  </p>
+
+  <p class="read-the-docs">
+    Ingeniero en sistemas
+  </p>
+</main>
+
+<style>
+  .logo {
+    height: 6em;
+    padding: 1.5em;
+    will-change: filter;
+  }
+  .logo:hover {
+    filter: drop-shadow(0 0 2em #646cffaa);
+  }
+  .logo.svelte:hover {
+    filter: drop-shadow(0 0 2em #ff3e00aa);
+  }
+  .read-the-docs {
+    color: #888;
+  }
+</style>
